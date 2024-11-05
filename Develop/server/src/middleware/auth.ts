98a2e4bd0638c4bfbbf96136c80ed02d4be15b05
khori moore <khorimoore23@gmail.com>
2024-11-05 10:43:14 -0800

    Completed authentication token for auth.ts file

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -2,9 +2,26 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
-  username: string;
+    username: string;
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access token is missing' });
+    }
+
+    try {
+        const secretKey = process.env.JWT_SECRET_KEY as string;
+        const decoded = jwt.verify(token, secretKey) as JwtPayload;
+
+        
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid or expired token' });
+    }
 };
